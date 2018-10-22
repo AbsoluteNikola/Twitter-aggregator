bb22426681d9cd0296bc212665c5845b09f305af
Danil Doroshin <ddddanil5555@gmail.com>
2018-10-22 17:14:52 +0300

    Twitter login

--- webapp/views.py
+++ webapp/views.py
@@ -3,5 +3,11 @@ from aggregator import Twit, User
 from flask import session, request, url_for, render_template
 
 @app.route('/')
+@app.route('/index')
+@app.route('/index.html')
 def index():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def error404(e):
+    return render_template("404.html"), 404
